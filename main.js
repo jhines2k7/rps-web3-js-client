1123e5c1ef7a1c3e101fcee3241b813ae016e30b
James Hines <jhines2k7@yahoo.com>
2023-11-07 12:37:47 -0600

    Catches a different version of an error in the Brave browser

--- main.js
+++ main.js
@@ -538,7 +538,7 @@ async function payStake(stakeUSD, contractAddress) {
 
       payStakeStatusP.innerText = "Check your account balance. Your wallet thinks you have insufficient funds. This " +
         " is sometimes due to a sudden increase in gas prices on the network. We've notified your opponent. Try again " +
-        "in a few minutes of refresh now to start a new game.";
+        "in a few minutes or refresh now to start a new game.";
 
       payStakeStatusP.style.color = 'red';
       payStakeStatusP.classList.remove('flashing');
@@ -572,7 +572,7 @@ async function payStake(stakeUSD, contractAddress) {
 
       payStakeStatusP.innerText = "Check your account balance. Metamask thinks you have insufficient funds. This " +
         " is sometimes due to a sudden increase in gas prices on the network. We've notified your opponent. Try again " +
-        "in a few minutes of refresh now to start a new game.";
+        "in a few minutes or refresh now to start a new game.";
 
       payStakeStatusP.style.color = 'red';
       payStakeStatusP.classList.remove('flashing');
