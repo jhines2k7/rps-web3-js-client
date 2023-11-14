fcf5e6c284189f0799db5a0b930b32b6e63b8d37
James Hines <jameshines10@gmail.com>
2023-11-14 22:08:03 +0000

    Forcing a build

--- main.js
+++ main.js
@@ -257,6 +257,7 @@ function registerSocketIOEventListeners() {
   socket.on('wager_declined', (data) => {
     console.log(`Wager declined by opponent in game ${data.game_id}`)
     yourWagerStatusP.innerText = '';
+    
     oppWagerStatusP.innerText = 'Your opponent declined your wager. Try to offer a different amount.';
     offerWagerBtn.disabled = false;
     wagerInput.disabled = false;
